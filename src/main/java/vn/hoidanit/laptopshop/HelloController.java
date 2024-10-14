cbc69cb58cdd16b202510b1fc9e5f140f1cea8cf
Manh Nguyen <manhbapper190704@gmail.com>
2024-10-14 17:04:17 +0700

    update-video 39-Spring Boot Devtool

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World with Nguyen Dang Manh-19/07/04!";
+        return "Hello World update";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page";
     }
 }
