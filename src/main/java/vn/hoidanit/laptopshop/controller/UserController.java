cb887a6d5bef3189af9770b3e5970cc1daf129cb
Manh Nguyen <manhbapper190704@gmail.com>
2024-10-29 09:34:08 +0700

    update video 53

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,18 +1,55 @@
+// package vn.hoidanit.laptopshop.controller;
+
+// import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.ui.Model;
+// import vn.hoidanit.laptopshop.service.UserService;
+// import org.springframework.web.bind.annotation.RequestMethod;
+
+// @Controller
+// public class UserController {
+//     private UserService userService;
+
+//     public UserController(UserService userService) {
+//         this.userService = userService;
+//     }
+
+//     @RequestMapping("/")
+//     public String getHomePage(Model model) {
+//         String test = this.userService.handHello();
+//         model.addAttribute("eric", test);
+//         model.addAttribute("hoidanit", "from controller with model");
+//         return "hello";
+//     }
+
+//     @RequestMapping("/admin/user")
+//     public String getUserPage(Model model) {
+//         model.addAttribute("newUser", new User());
+//         return "admin/user/create";
+//     }
+
+//     @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+//     public String createUserPage(Model model) {
+//         System.out.println("run here");
+//         return "hello";
+//     }
+// }
+
 package vn.hoidanit.laptopshop.controller;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import org.springframework.ui.Model;
-import vn.hoidanit.laptopshop.service.UserService;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
 public class UserController {
+
     private UserService userService;
 
     public UserController(UserService userService) {
@@ -26,20 +63,16 @@ public class UserController {
         model.addAttribute("hoidanit", "from controller with model");
         return "hello";
     }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
+        System.out.println(" run here " + hoidanit);
+        return "hello";
+    }
 }
-
-// @RestController
-// public class UserController {
-
-// // DI : dependency injection
-// private UserService userService;
-
-// public UserController(UserService userService) {
-// this.userService = userService;
-// }
-
-// @GetMapping("")
-// public String getHomePage() {
-// return this.userService.handHello();
-// }
-// }
